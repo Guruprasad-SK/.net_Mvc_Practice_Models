bcbfd7ea4f21c2162925d743e721342b178b29c4
Guruprasad S K <115786145+Guruprasad-SK@users.noreply.github.com>
2023-10-07 16:33:32 +0530

    Add files via upload

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ModelsDemo.Models;
+
+namespace ModelsDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            var data = GetStudent();
+            return View(data);
+        }
+        private Student GetStudent()
+        {
+            return new Student()
+            {
+                Id=1,
+                Name="Vinay",
+                Address="Bangalore"
+            };
+        }
+    }
+}
